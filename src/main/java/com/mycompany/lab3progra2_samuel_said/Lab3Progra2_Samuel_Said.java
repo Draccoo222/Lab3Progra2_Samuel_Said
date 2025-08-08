8660b9db394e6b1423267bae7eb3fdb5a6a635ef
Samuel Vasquez <83726280+Draccoo222@users.noreply.github.com>
2025-08-08 08:41:55 -0600

    prueba

---
+++ src/main/java/com/mycompany/lab3progra2_samuel_said/Lab3Progra2_Samuel_Said.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lab3progra2_samuel_said;
+
+/**
+ *
+ * @author unwir
+ */
+public class Lab3Progra2_Samuel_Said {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
