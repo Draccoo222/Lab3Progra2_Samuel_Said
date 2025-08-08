7ca1091044afa0475d954e34b1194f89814b5435
Samuel Vasquez <83726280+Draccoo222@users.noreply.github.com>
2025-08-08 09:10:25 -0600

    baseBotones

---
+++ src/main/java/com/mycompany/lab3progra2_samuel_said/BotonPlayer.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.lab3progra2_samuel_said;
+
+/**
+ *
+ * @author unwir
+ */
+public class BotonPlayer extends Boton {
+
+    public BotonPlayer(int columna, int fila) {
+        super(columna, fila);
+    }
+    
+    
+}
---
+++ src/main/java/com/mycompany/lab3progra2_samuel_said/Tablero.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.lab3progra2_samuel_said;
+
+import javax.swing.JPanel;
+import java.util.ArrayList;
+/**
+ *
+ * @author unwir
+ */
+public class Tablero extends JPanel {
+    
+    
+    
+    
+    
+}
